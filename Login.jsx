6c5ab4d25e1a92e8830453ed95ecd38ceba02066
Sumit Parise <82759795+SumitParise@users.noreply.github.com>
2024-08-12 16:00:19 +0530

    Update  Login Component

--- Login.jsx
+++ Login.jsx
@@ -1,88 +1,174 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Alert from "@mui/material/Alert";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validationSchema: Yup.object({
-      email: Yup.string()
-        .email('Invalid email address')
-        .required('Required'),
-      password: Yup.string()
-        .required('Required'),
-    }),
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+
+
+const BASE_URL = "http://localhost:8000/api/v1";
+
+const theme = createTheme();
+
+export default function Login({ handleHaveAccount }) {
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+
+    const data = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    try {
+      const response = await axios({
+        method: "post",
+        url: BASE_URL + "/users/login",
+        data: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.data) {
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("id", response.data.user.id);
+        localStorage.setItem("loggedInUser", response.data.user.name);
+        localStorage.setItem("email", response.data.user.email);
+        localStorage.setItem("isLoggedIn", true);
+        localStorage.setItem("role", response.data.user.role.toUpperCase());
+        if(response.data.user.role.toUpperCase()==="DAYCARE_OWNER"){
+          const daycareResponse = await axios({
+            method: "GET",
+            url: BASE_URL + "/daycare/my-daycare/"+response.data.user.id,
+            headers: { "Content-Type": "application/json" },
+            Authorization: "Bearer " + response.data.token,
+          });
+          if(daycareResponse.data?.id){
+            navigate(`/daycare-dashboard/${daycareResponse.data.id}`)
+          }else{
+            navigate("/daycare-registration")
+          }
+        }else{
+          navigate("/");
+        }
+                
+      }
+    } catch (err) {
+      setError(true);
+      setErrorMessage("Login Failed");
+      setTimeout(() => setError(false), 5000);
+    }
+  };
 
   return (
-    <Container className="mt-5">
-      <Row>
-        <Col md={6}>
-          <h2 style={{ padding: '30px', color: 'blue' }}>Login</h2>
-          <Form onSubmit={formik.handleSubmit}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                name="email"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
-                isInvalid={formik.touched.email && formik.errors.email}
-              />
-              <Form.Control.Feedback type="invalid">
-                {formik.errors.email}
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                name="password"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.password}
-                isInvalid={formik.touched.password && formik.errors.password}
-              />
-              <Form.Control.Feedback type="invalid">
-                {formik.errors.password}
-              </Form.Control.Feedback>
-            </Form.Group>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {error && <Alert severity="error">{errorMessage}</Alert>}
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                sx={{ marginTop: 3, marginLeft: 5 }}
+              >
+                <Grid item xs={6}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        onChange={() => setShowPassword(!showPassword)}
+                        value={showPassword}
+                        color="primary"
+                      />
+                    }
+                    label="Show Password"
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Button
+                    onClick={() => navigate("/reset-password")}
+                    variant=""
+                    sx={{ color: "blue" }}
+                  >
+                    Forgot Password
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
             <Button
-              style={{ marginTop: '3rem' }}
-              className='position-relative bottom-0 start-50 translate-middle'
-              variant="outline-primary"
               type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
               Login
             </Button>
-          </Form>
-          <div className="mt-3">
-            <span>Don't have an account? </span>
-            <Link to="/register">Register here</Link>
-          </div>
-        </Col>
-
-        <Col md={5}>
-          <img
-            style={{ marginLeft: '5rem', height: '500px' }}
-            src={process.env.PUBLIC_URL + '/login.png'}
-            alt="Login"
-          />
-        </Col>
-      </Row>
-    </Container>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Button onClick={() => handleHaveAccount(false)}>
+                  <Link href="#" variant="body2">
+                    Don't have an account? Register
+                  </Link>
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
-export default Login;
+<button onClick={() => localStorage.clear()}>Log Out</button>;

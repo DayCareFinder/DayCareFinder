ccbb0f1e1e735a16447167d3c51589ccf0d4cf51
RutujaPatil27 <163537712+PatilRutuja27@users.noreply.github.com>
2024-08-15 13:35:10 +0530

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,8 +0,0 @@
-package tda.darkarmy.daycare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tda.darkarmy.daycare.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}

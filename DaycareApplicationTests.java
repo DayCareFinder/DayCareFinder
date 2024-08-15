af3089add2b8615e5d0667f6fbf3b3420bb34663
Sumit Parise <82759795+SumitParise@users.noreply.github.com>
2024-08-16 01:12:13 +0530

    DayCare Test Class Added

---
+++ DaycareApplicationTests.java
@@ -0,0 +1,15 @@
+package tda.darkarmy.daycare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = {DaycareApplication.class})
+class DaycareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

375e1b5ff0826876e78854b1a3990280cf05bbe0
RutujaPatil27 <163537712+PatilRutuja27@users.noreply.github.com>
2024-08-15 13:35:37 +0530

    Delete ReviewRepository.java

--- ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package tda.darkarmy.daycare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tda.darkarmy.daycare.model.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
